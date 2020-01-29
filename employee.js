b6ec389a74daddfba5454c8edfe5722b1b0059b5
rayna williams <Raynawilliams123@gmail.com>
2020-01-28 18:12:31 -0700

    added my inquirer prompts

--- employee.js
+++ employee.js
@@ -12,10 +12,29 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "RKW@1993!",
-  database: ""
+  database: "employeeDB"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
   runSearch();
 });
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "Add an Employee",
+        "Add a Department",
+        "Add a new Role",
+        "View Departments",
+        "View Roles",
+        "View Employees",
+        "Update an existing employees role",
+        "Find artists with a top song and top album in the same year"
+      ]
+    })
+}
